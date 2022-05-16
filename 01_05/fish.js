2c197cc7b048ab54dbbadda40c5ed062da2268f3
Mike Mleczko <mike.r.mleczko@exxonmobil.com>
2022-05-16 08:10:43 -0500

    trying construct in js for HTML

--- 01_05/fish.js
+++ 01_05/fish.js
@@ -2,32 +2,55 @@
 
 will be giving some properties so describe a fish
 */
-
-class Fish{
-    constructor(
-    // define parameters
-        name,
-        size,
-        aggression,
-        purpose,
-        reefSafe,
-    ){
-      
-        // Define Properties here , class should be declared above
-
-        this.name = name;
-        this.size = size;
-        this.aggression = aggression;
-        this.purpose = purpose;
-        this.reefSafe = reefSafe;
-
-
-    };    
-    setSize(current_size) {
-        this.size = current_size;
-    } 
+const updateBackpack = (update) => {
+    let main = document.querySelector("main"); // main is an element
+    main.innerHTML = markup(backpack);
     
+    console.info(update);
+  };
+  
+const fish = {
+    name: "Clownfish",
+    size: "Small",
+    aggression: "Peaceful",
+    purpose: "Asthetics",
+    reefSafe: true,
+    toggleSafe: function (fishStatus) {
+    this.fishChange = fishStatus;
+    updateReefSafe(`Fish status changed.`);
+  },
+}
 
+// {
+      
+//         // Define Properties here , class should be declared above
+
+//         // this.name = name;
+//         // this.size = size;
+//         // this.aggression = aggression;
+//         // this.purpose = purpose;
+//         // this.reefSafe = reefSafe;
+
+
+//     };    
+  
+
+    const markup = (fish) => {
+        return `
+        <div>
+          <h3>${fish.name}</h3>
+          <ul>
+            <li>Size: ${fish.size}</li>
+            <li>Aggression: ${fish.aggression}</li>
+            <li>Purpose: ${fish.purpose}</li>
+            <li>Reef Safe: ${fish.fishChange ? "Peaceful" : "Aggressive"}</li>
+          </ul>
+        </div>
+      `;
+      };
 
 
 }
+const main = document.createElement("main");
+main.innerHTML = markup(fish);
+document.body.appendChild(main);
